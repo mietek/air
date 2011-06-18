04a846087e8386b35c2312b55195425fd1b40692
jinjing <nfjinjing@gmail.com>
2011-06-18 17:44:11 +0800

    export default

--- src/Air/Env.hs
+++ src/Air/Env.hs
@@ -1,14 +1,17 @@
-module Air.Env (
+module Air.Env
+(
     module Air.Light
   , module Prelude
   , module Air.Data.Monoid
   , module Air.Control.Monad.ListBuilder
   , module Air.Control.Monad.ObjectBuilder
+  , module Air.Data.Default
   
 ) where
 
 import Air.Light
 import Prelude hiding ((.), (>), (<), (^), (/), (-), (+), length, drop, take, splitAt, replicate, (!!))
 import Air.Data.Monoid ((+))
+import Air.Data.Default (Default, def)
 import Air.Control.Monad.ListBuilder
 import Air.Control.Monad.ObjectBuilder
