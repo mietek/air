d889ad7ef015f34b9f624b1124a814cb2508aa02
jinjing <nfjinjing@gmail.com>
2011-07-25 20:51:27 +0800

    add mangaged object code

--- src/Air/Env.hs
+++ src/Air/Env.hs
@@ -13,5 +13,6 @@ import Air.Light
 import Prelude hiding ((.), (>), (<), (^), (/), (-), (+), length, drop, take, splitAt, replicate, (!!))
 import Air.Data.Monoid ((+))
 import Air.Data.Default (Default, def)
+import Air.Data.Managed (Managed, initialize, destroy, with_managed_object)
 import Air.Control.Monad.ListBuilder
 import Air.Control.Monad.ObjectBuilder
