5655cfbec7f552cad0159fb88f74a6271d3fb043
Jinjing Wang <nfjinjing@gmail.com>
2013-06-22 12:25:01 +0800

    fix lib

--- src/Air/SimpleMath.hs
+++ src/Air/SimpleMath.hs
@@ -20,7 +20,7 @@ encode :: (Eq a) => [a] -> [(Int, a)]
 encode xs = xs.L.group.map (length &&& head)
 
 decode :: [(Int, a)] -> [a]
-decode xs = xs.map(\(l,x) -> l.times x).join'
+decode xs = xs.map(\(l,x) -> l.times x).concat
 
 hist :: (Num e, A.Ix i) =>  (i, i) -> [i] -> A.Array i e
 hist bnds ns = A.accumArray (+) 0 bnds [(n, 1) | n <- ns, A.inRange bnds n]
